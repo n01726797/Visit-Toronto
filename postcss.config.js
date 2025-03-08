3b1836553e802be03d112a27f3d365070412b71a
Sukhman Brar <n01726797@humber.ca>
2025-03-08 17:59:28 -0500

    Added tailwind and diasui to homepage

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
+// postcss.config.js
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+      require('daisyui'),
+    ]
+  }
+  
   
